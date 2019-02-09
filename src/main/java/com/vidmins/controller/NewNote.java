52124eaeb74de5da2f959f4c80bd74a3011cab47
Curtis Moore <cwmoore@madisoncollege.edu>
2019-02-09 16:03:29 -0600

    Begin to add logging to new note servlet

--- src/main/java/com/vidmins/controller/NewNote.java
+++ src/main/java/com/vidmins/controller/NewNote.java
@@ -2,6 +2,8 @@ package com.vidmins.controller;
 
 import com.vidmins.entity.Note;
 import com.vidmins.persistence.NoteData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -23,6 +25,16 @@ import java.util.*;
 
 public class NewNote extends HttpServlet {
 
+    private int previousDigit;
+    private Logger logger;
+
+    /**
+     * Initialize variables
+     */
+    public void init() {
+        logger = LogManager.getLogger(this.getClass());
+        logger.info("Starting NewNote servlet");
+    }
     /**
      * Handle a POST request (new note)
      *
@@ -36,6 +48,8 @@ public class NewNote extends HttpServlet {
 
         NoteData noteData = new NoteData();
 
+        logger.debug(req.getParameterMap());
+
         if (req.getParameter("noteId") != null &&
                 req.getParameter("label") != null &&
                 req.getParameter("note_text") != null &&
@@ -48,8 +62,10 @@ public class NewNote extends HttpServlet {
             int insertId = noteData.setNewNote(noteFields);
 
             req.getSession().setAttribute("noteId", insertId);
+            logger.debug("New Note succeeded: #" + insertId);
         } else {
             // error messages
+            logger.debug("New Note failed");
         }
 
         resp.sendRedirect("/loadClient");
