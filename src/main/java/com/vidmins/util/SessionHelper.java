cc2f0c4aebfbe6bcb50c52740e667163799e5780
Curtis <cwmoore@madisoncollege.edu>
2019-05-09 14:35:19 -0500

    Invalidate session vars after delete

---
+++ src/main/java/com/vidmins/util/SessionHelper.java
@@ -0,0 +1,31 @@
+package com.vidmins.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionHelper {
+
+    Logger logger = LogManager.getLogger(this.getClass());
+
+    public void resetAll(HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+
+        String[] attributes = {
+                "currentDirectory"
+                , "directory"
+                , "currentVideo"
+                , "video"
+                , "currentNote"
+                , "note"
+        };
+
+        for (String attribute : attributes) {
+            logger.debug("Setting " + attribute + " to null.");
+            session.setAttribute(attribute, null);
+        }
+    }
+}
