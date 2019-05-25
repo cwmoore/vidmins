3f3da9e2f7090b4a47d42916fa4a4bcb3b841f18
Curtis <cwmoore@madisoncollege.edu>
2019-05-25 12:53:53 -0500

    Undo first attempt at hashed ids

--- src/main/java/com/vidmins/entity/Note.java
+++ src/main/java/com/vidmins/entity/Note.java
@@ -19,7 +19,7 @@ public class Note implements java.io.Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private int id;
-    
+
     private String label;
     private String text;
     
