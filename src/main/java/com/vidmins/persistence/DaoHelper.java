6b9afca883705c8647995fa225ae53259f123e45
Curtis <cwmoore@madisoncollege.edu>
2019-05-27 14:43:58 -0500

    Reenable httpsession dao method

--- src/main/java/com/vidmins/persistence/DaoHelper.java
+++ src/main/java/com/vidmins/persistence/DaoHelper.java
@@ -42,51 +42,51 @@ public class DaoHelper {
         logger = LogManager.getLogger(this.getClass());
         logger.debug("loadHelpers(request)");
 
-        loadHelpers();
-//
-//        if (user == null) {
-//            if (request.getSession().getAttribute("userDao") == null) {
-//                user = new GenericDao<>(User.class);
-//                request.getSession().setAttribute("userDao", user);
-//            } else {
-//                user = (GenericDao<User>) request.getSession().getAttribute("userDao");
-//            }
-//        }
-//
-//        if (directory == null) {
-//            if (request.getSession().getAttribute("directoryDao") == null) {
-//                directory = new GenericDao<>(Directory.class);
-//                request.getSession().setAttribute("directoryDao", directory);
-//            } else {
-//                directory = (GenericDao<Directory>) request.getSession().getAttribute("directoryDao");
-//            }
-//        }
-//
-//        if (video == null) {
-//            if (request.getSession().getAttribute("videoDao") == null) {
-//                video = new GenericDao<>(Video.class);
-//                request.getSession().setAttribute("videoDao", video);
-//            } else {
-//                video = (GenericDao<Video>) request.getSession().getAttribute("videoDao");
-//            }
-//        }
-//
-//        if (youTubeVideo == null) {
-//            if (request.getSession().getAttribute("youTubeVideoDao") == null) {
-//                youTubeVideo = new GenericDao<>(YouTubeVideo.class);
-//                request.getSession().setAttribute("youTubeVideoDao", youTubeVideo);
-//            } else {
-//                youTubeVideo = (GenericDao<YouTubeVideo>) request.getSession().getAttribute("youTubeVideoDao");
-//            }
-//        }
-//
-//        if (note == null) {
-//            if (request.getSession().getAttribute("noteDao") == null) {
-//                note = new GenericDao<>(Note.class);
-//                request.getSession().setAttribute("noteDao", note);
-//            } else {
-//                note = (GenericDao<Note>) request.getSession().getAttribute("noteDao");
-//            }
-//        }
+        //loadHelpers();
+
+        if (user == null) {
+            if (request.getSession().getAttribute("userDao") == null) {
+                user = new GenericDao<>(User.class);
+                request.getSession().setAttribute("userDao", user);
+            } else {
+                user = (GenericDao<User>) request.getSession().getAttribute("userDao");
+            }
+        }
+
+        if (directory == null) {
+            if (request.getSession().getAttribute("directoryDao") == null) {
+                directory = new GenericDao<>(Directory.class);
+                request.getSession().setAttribute("directoryDao", directory);
+            } else {
+                directory = (GenericDao<Directory>) request.getSession().getAttribute("directoryDao");
+            }
+        }
+
+        if (video == null) {
+            if (request.getSession().getAttribute("videoDao") == null) {
+                video = new GenericDao<>(Video.class);
+                request.getSession().setAttribute("videoDao", video);
+            } else {
+                video = (GenericDao<Video>) request.getSession().getAttribute("videoDao");
+            }
+        }
+
+        if (youTubeVideo == null) {
+            if (request.getSession().getAttribute("youTubeVideoDao") == null) {
+                youTubeVideo = new GenericDao<>(YouTubeVideo.class);
+                request.getSession().setAttribute("youTubeVideoDao", youTubeVideo);
+            } else {
+                youTubeVideo = (GenericDao<YouTubeVideo>) request.getSession().getAttribute("youTubeVideoDao");
+            }
+        }
+
+        if (note == null) {
+            if (request.getSession().getAttribute("noteDao") == null) {
+                note = new GenericDao<>(Note.class);
+                request.getSession().setAttribute("noteDao", note);
+            } else {
+                note = (GenericDao<Note>) request.getSession().getAttribute("noteDao");
+            }
+        }
     }
 }
